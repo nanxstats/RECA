3af6f18d0fe53adc415d35d2d8920f5848f10fb9
Nan Xiao <road2stat@gmail.com>
2018-05-16 14:11:41 -0400

    remove version from rd following kurt hornik's email

--- R/RECA-package.R
+++ R/RECA-package.R
@@ -3,8 +3,9 @@
 #' The RECA package offers an R implementation for
 #' Relevant Component Analysis (RCA).
 #'
-#' \tabular{ll}{ Package: \tab RECA\cr Type: \tab Package\cr
-#' Version: \tab 1.3\cr License: \tab GPL-3\cr }
+#' \tabular{ll}{ Package: \tab RECA\cr
+#' Type: \tab Package\cr
+#' License: \tab GPL-3\cr }
 #'
 #' @name RECA-package
 #' @docType package
