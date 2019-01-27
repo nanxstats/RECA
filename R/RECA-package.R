7697ac97c6122339b3e1f981a253365fab03871e
Nan Xiao <road2stat@gmail.com>
2019-01-27 14:00:59 -0500

    use roxygen2 for package page

--- R/RECA-package.R
+++ R/RECA-package.R
@@ -1,18 +1,2 @@
-#' Relevant Component Analysis
-#'
-#' The RECA package offers an R implementation for
-#' Relevant Component Analysis (RCA).
-#'
-#' \tabular{ll}{
-#' Package: \tab RECA\cr
-#' Type: \tab Package\cr
-#' License: \tab GPL-3}
-#'
-#' @name RECA-package
-#' @docType package
-#'
-#' @note
-#' Bug reports and feature requests can be sent to
-#' \url{https://github.com/road2stat/RECA/issues}.
-#'
-NULL
+#' @keywords internal
+"_PACKAGE"
